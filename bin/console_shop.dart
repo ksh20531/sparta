d871de1e4758ddb7b0a05a8334db002f842fa011
김서후 <ksh20531@gmail.com>
2024-10-31 13:16:11 +0900

    프로젝트 생성

---
+++ bin/console_shop.dart
@@ -0,0 +1,5 @@
+import 'package:console_shop/console_shop.dart' as console_shop;
+
+void main(List<String> arguments) {
+  print('Hello world: ${console_shop.calculate()}!');
+}
---
+++ test/console_shop_test.dart
@@ -0,0 +1,8 @@
+import 'package:console_shop/console_shop.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
