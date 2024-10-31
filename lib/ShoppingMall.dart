82a126707ea68ad626b7cab2acf17784a960fd3e
김서후 <ksh20531@gmail.com>
2024-10-31 13:45:27 +0900

    20241031 ShoppingMall 주석 추가

--- lib/ShoppingMall.dart
+++ lib/ShoppingMall.dart
@@ -2,5 +2,14 @@ import 'package:console_shop/Product.dart';
 
 class ShoppingMall {
   List<Product> productList = [];
-  int totalPrice = 0;10
+  int totalPrice = 0;
+
+  /// 상품 목록 출력
+  showProducts() {}
+
+  /// 상품을 장바구니에 추가
+  addToCart() {}
+
+  /// 장바구니에 담긴 상품의 상품 가격의 총 합
+  showTotal() {}
 }
