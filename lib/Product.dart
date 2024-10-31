0a595a54490ee294135ac363ea6f85976cd06b22
김서후 <ksh20531@gmail.com>
2024-10-31 14:35:51 +0900

    20241031 Product 생성자 방식 변경

--- lib/Product.dart
+++ lib/Product.dart
@@ -1,4 +1,9 @@
 class Product {
   String productName = '';
   int productPrice = 0;
+
+  Product(this.productName, this.productPrice) {
+    this.productName = productName;
+    this.productPrice = productPrice;
+  }
 }
